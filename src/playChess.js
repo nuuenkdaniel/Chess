30e6cacc505614b78e21ececa0c13fdf45521d54
nuuenkdaniel <lewdaniel2@gmail.com>
2023-05-20 17:11:48 -0400

    began making setup functions in playChess

--- src/playChess.js
+++ src/playChess.js
@@ -1,1 +1,21 @@
-let board = [];
+let tiles = [];
+let boardLength = 8;
+let boardWidth = 8;
+
+function tileSetUp(){
+    let color = "FFFFFF";
+    let otherColor = "A52A2A";
+    for(let i = 0; i < boardWidth; i++){
+        tile[i] = [];
+        for(let j = 0; j < boardLength; j++){
+            tile[i][j] = new Tile(j,i,color);
+            let tempColor = color;
+            color = otherColor;
+            otherColor = tempColor;
+        }
+    }
+}
+
+function blackSetUp(){
+    let 
+}
