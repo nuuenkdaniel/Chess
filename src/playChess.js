217dd40e5087f23f2744de6479e79797234b0928
nuuenkdaniel <lewdaniel2@gmail.com>
2023-05-20 17:26:17 -0400

    finished setup func and added piece king

--- src/playChess.js
+++ src/playChess.js
@@ -17,5 +17,31 @@ function tileSetUp(){
 }
 
 function blackSetUp(){
-    let 
+    let color = "A52A2A";
+    for(let i = 0; i < boardLength; i++){
+        tile[1][i] = new Pawn(color, true);
+    }
+    tile[0][0] = new Rooke(color);
+    tile[0][1] = new Knight(color);
+    tile[0][2] = new Bishop(color);
+    tile[0][3] = new Queen(color);
+    tile[0][4] = new King(color);
+    tile[0][5] = new Bishop(color);
+    tile[0][6] = new Knight(color);
+    tile[0][7] = new Rooke(color);
+}
+
+function whiteSetUp(){
+    let color = "FFFFFF";
+    for(let i = 0; i < boardLength; i++){
+        tile[6][i] = new Pawn(color, true);
+    }
+    tile[7][0] = new Rooke(color);
+    tile[7][1] = new Knight(color);
+    tile[7][2] = new Bishop(color);
+    tile[7][3] = new Queen(color);
+    tile[7][4] = new King(color);
+    tile[7][5] = new Bishop(color);
+    tile[7][6] = new Knight(color);
+    tile[7][7] = new Rooke(color);
 }
