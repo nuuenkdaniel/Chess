be8f4ae8124838564eaf16357400b6764d4cf564
nuuenkdaniel <lewdaniel2@gmail.com>
2023-05-20 19:46:52 -0400

    created a possible moves function

--- src/playChess.js
+++ src/playChess.js
@@ -16,7 +16,8 @@ function tileSetUp(){
     }
 }
 
-function blackSetUp(){
+function defaultPieceSetUp(){
+    //black pieces set up
     let color = "A52A2A";
     for(let i = 0; i < boardLength; i++){
         tile[1][i] = new Pawn(color, true);
@@ -29,10 +30,9 @@ function blackSetUp(){
     tile[0][5] = new Bishop(color);
     tile[0][6] = new Knight(color);
     tile[0][7] = new Rooke(color);
-}
 
-function whiteSetUp(){
-    let color = "FFFFFF";
+    //white pieces set up
+    color = "FFFFFF";
     for(let i = 0; i < boardLength; i++){
         tile[6][i] = new Pawn(color, true);
     }
@@ -44,4 +44,23 @@ function whiteSetUp(){
     tile[7][5] = new Bishop(color);
     tile[7][6] = new Knight(color);
     tile[7][7] = new Rooke(color);
+}
+
+function possibleMoves(tile){
+    let possibleTiles = [];
+    switch(tile.getPiece()){
+        case Pawn:
+            let tiles = tile.getPiece().isFirstMove()? 2 : 1;
+            for(let i = 1; i <= tiles; i++){
+                if(tile[tile.tileY()-i, tile.tileX()].isTileOccupied()){
+                    break;
+                }
+                possibleTiles = tile[tile.tileY()-i, tile.tileX()].tile
+            }
+    }
+}
+
+function move(x1,y1,x2,y2){
+    tile[y2][x2] = tile[y1][x1].getPiece();
+    tile[y1][x1].rmPiece();
 }
